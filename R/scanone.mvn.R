ffa9ae495f97614a521a536e7d06f4713d705bcf
Jianan Tian <tian@stat.wisc.edu>
2014-01-20 21:12:24 -0600

    scanone: add input checking.

--- R/scanone.mvn.R
+++ R/scanone.mvn.R
@@ -1,23 +1,23 @@
-##' Multivariate normal genome scan with a single QTL model
+##' Multivariate normal genome scan with a single QTL model.
 ##' 
-##' Genome scan with a single QTL model, with possible allowance for
+##' Genome scan with a single QTL model, with allowance for possible
 ##' covariates, using multivariate normal model for the multiple
 ##' traits.
 ##'
 ##' @param Y matrix of multiple traits with n rows.
-##' @param cross An object of class \code{cross}. See \code{read.cross} for
-##' details.
+##' @param cross An object of class \code{cross}. See
+##' \code{read.cross} for details.
 ##' @param chr Optional vector indicating the chromosomes for which
 ##' LOD scores should be calculated.  This should be a vector of
 ##' character strings referring to chromosomes by name; numeric values
 ##' are converted to strings.
 ##' @param addcov Additive covariates.
 ##' @param intcov Interactive covariates.
-##' @param tol Tolerance value for the \code{qr} decomposition in \code{lm}
-##' fitting.
+##' @param tol Tolerance value for the \code{qr} decomposition in
+##' \code{lm} fitting.
 ##' @return A data.frame whose first column contains the chromosome
-##' IDs, second column contains cM positions, third column contains the
-##' LOD scores.
+##' IDs, second column contains cM positions, third column contains
+##' the LOD scores.
 ##' @keywords QTL
 ##' @seealso qtl::scanone
 ##' @export
@@ -28,23 +28,28 @@
 ##' p <- 5
 ##' Y <- matrix(rnorm(n*p),n,p)
 ##' scanone.mvn(Y, hyper)
+
 scanone.mvn <- function(Y, cross, chr=NULL, addcov=NULL, intcov=NULL, tol=1e-7){
 
   ## checking inputs...
   if(!is.matrix(Y)) stop("Y need to be a matrix.")
+  if(!is.matrix(addcov)) stop("addcov need to be a matrix.")
+  if(!is.matrix(intcov)) stop("intcov need to be a matrix.")
   if(class(cross)[2] != "cross") stop("cross need to be of class cross")
-  nobs <- nrow(cross$pheno)
-  n <- nrow(Y)
+  n <- nrow(cross$pheno)
   p <- ncol(Y)
-  if(n != nobs) stop("number of obs. in cross and Y not same.")
+  if(n != nrow(Y)) stop("number of obs. in cross and Y not same.")
   if(p < 1) stop("Y should be a matrix with more than one columns")
-
+  if(n != nrow(addcov)) stop("number of obs. in cross and addcov not same.")
+  if(n != nrow(intcov)) stop("number of obs. in cross and intcov not same.")
+  
   if(missing(chr)){
     chr <- names(cross$geno)
   }else{
     cross <- subset(cross, chr)
   }
 
+  ## todo-need to deal with X-chr
   chrclass <- sapply(cross$geno,class)
   genoprob <- pull.genoprob(cross,chr=chr)
   if(attr(cross,"class")[1] == "bc"){
