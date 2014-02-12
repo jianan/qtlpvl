ca84893178afe3c051e903d28947de425a991828
Jianan Tian <jiant17@gmail.com>
2014-02-12 14:55:56 -0600

    add one more usage.

--- R/plotGenetpattern.R
+++ R/plotGenetpattern.R
@@ -3,8 +3,9 @@
 ##' QTL additive effect is defined as a=(RR-BB)/2;
 ##' QTL dominant effect is defined as d=BR-(BB+RR)/2.
 ##' 
+##' @inheritParams scanone.mvn
 ##' @param Y matrix, columns are quantitative traits maps to the same position. 
-##' @param genotype QTL genotype for the traits.
+##' @param genotype QTL genotype for the common QTL.
 ##' @return a plot of QTL dominance effect versus QTL additive effect.
 ##' @export
 ##' @examples
@@ -24,20 +25,56 @@
 ##' G <- cbind(G1, G2*(-2))
 ##' Y <- matrix(rnorm(n*p),n,p)
 ##' Y <- Y + G
-##' plotGenetpattern(Y, genotype1)
+##' plotGenetpattern(Y, genotype=genotype1)
+##' plotGenetpattern(Y, cross=listeria, chr=chr)
 
-plotGenetpattern <- function(Y, genotype){
-  if(!is.matrix(Y)) stop("Y need to be a matrix of quantitative traits")
-  if(length(genotype) != nrow(Y)) stop("number of objects in 'Y' and 'genotype' not same. ")
-  if(length(unique(genotype)) != 3) stop("genotype should have 3 levels. ")
+
+plotGenetpattern <- function(Y, genotype=NULL, cross, chr,
+                             addcovar=NULL, intcovar=NULL, LOD.threshold=3,  ...){
   
-  gn <- as.numeric(genotype)
-  eff1 <- apply(Y[which(gn==1), ], 2, mean)
-  eff2 <- apply(Y[which(gn==2), ], 2, mean)
-  eff3 <- apply(Y[which(gn==3), ], 2, mean)
+  if(!is.matrix(Y)) stop("Y need to be a matrix of quantitative traits")
+  if(!missing(genotype) & length(genotype) != nrow(Y)) stop("number of objects in 'Y' and 'genotype' not same. ")
+  if(!missing(genotype) & length(unique(genotype)) != 3) stop("genotype should have 3 levels. ")
+  
+  if(missing(genotype)){  ## if genotype is not given, use QTL for each trait.
+    
+    n <- nrow(Y)
+    p <- ncol(Y)
+    p1 <- ncol(cross$pheno)
+    cross$pheno <- data.frame(cross$pheno, Y)
+    if(!is.null(colnames(Y))) names(cross$pheno)[p1+(1:p)] <- colnames(Y)
+    if (!("prob" %in% names(cross[[c("geno",1)]]))){
+      warning("First running calc.genoprob.")
+      cross <- calc.genoprob(cross)
+    }
+    out <- scanone(cross, pheno.col=p1+(1:p), method="hk", chr=chr, 
+                   addcovar=addcovar, intcovar=intcovar)
+    maxPOSind <- apply(out[, -(1:2)], 2, which.max)
+    
+    step <- attr(cross[[c("geno",chr,"prob")]],"step")
+    off.end <- attr(cross[[c("geno",chr,"prob")]],"off.end")
+    error.prob <- attr(cross[[c("geno",chr,"prob")]],"error.prob") 
+    map.function <- attr(cross[[c("geno",chr,"prob")]],"map.function")
+    stepwidth <- attr(cross[[c("geno",chr,"prob")]],"stepwidth")
+    geno <- argmax.geno(cross, step, off.end, error.prob, map.function, stepwidth)
+    geno <- pull.argmaxgeno(geno, chr=chr)
+    
+    eff1 <- eff2 <- eff3 <- numeric(p)
+    for(i in 1:p){
+      eff1[i] <- mean(Y[geno[, maxPOSind[i]]==1, i], na.rm=TRUE)
+      eff2[i] <- mean(Y[geno[, maxPOSind[i]]==2, i], na.rm=TRUE)
+      eff3[i] <- mean(Y[geno[, maxPOSind[i]]==3, i], na.rm=TRUE)
+    }
+  }else{
+    gn <- as.numeric(genotype)
+    eff1 <- apply(Y[which(gn==1), ], 2, mean, na.rm=TRUE)
+    eff2 <- apply(Y[which(gn==2), ], 2, mean, na.rm=TRUE)
+    eff3 <- apply(Y[which(gn==3), ], 2, mean, na.rm=TRUE)
+  }  
   Eff <- cbind(eff1, eff2, eff3)
   a <- (Eff[, 3] - Eff[, 1])/2
-  d <- Eff[, 2] - (Eff[, 3] + Eff[, 1])/2 
+  d <- Eff[, 2] - (Eff[, 3] + Eff[, 1])/2
+
   lim <- max(abs(c(a,d)))
   xlim <- ylim <- c(-lim, lim)*1.1
   plot(x=a, y=d, pch=20, xlim=xlim, ylim=ylim, 
