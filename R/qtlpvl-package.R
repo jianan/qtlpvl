76dda998d3f6e2e5567b581586a9b2b7af003101
Jianan Tian <jiant17@gmail.com>
2014-05-01 15:54:04 -0500

    import Rcc and RcppEigen.

--- R/qtlpvl-package.R
+++ R/qtlpvl-package.R
@@ -1,10 +1,11 @@
-#' qtlpvl
-#' 
-#' QTL: test of pleiotrophy vs. close linkage
-#'
-#'
-#' @import qtl
-#' @name qtlpvl
-#' @useDynLib qtlpvl
-#' @docType package
+##' qtlpvl
+##' 
+##' QTL: test of pleiotrophy vs. close linkage
+##'
+##' @import qtl
+##' @import Rcpp
+##' @import RcppEigen
+##' @name qtlpvl
+##' @useDynLib qtlpvl
+##' @docType package
 NULL
