4371d8b5ba9d7c20d4404660202686cfde4be814
Jianan Tian <jiant17@gmail.com>
2014-05-13 16:14:42 -0500

    add para ... to plottrace, so the plot is modifiable(eg. change ylim)

--- R/plottrace.R
+++ R/plottrace.R
@@ -14,15 +14,17 @@ plottrace <- function(x)
 ##' changes as the cutting point moves.
 ##'
 ##' @param x object of class "testpleio.1vs2"
+##' @param ... parameters to be passed through to plotting functions.
 ##' @export
-plottrace.testpleio.1vs2 <- function(x){
+plottrace.testpleio.1vs2 <- function(x, ...){
   
   object <- x
   if (!any(class(object) == "testpleio.1vs2")) 
       stop("Input should have class \"testpleio.1vs2\".")
   t <- object$LODdiff.trace
   t[t<0] <- 0
-  plot(t, xlab="i.cut", ylab="LODdiff", type="b")
+  
+  plot(t, xlab="i.cut", ylab="LODdiff", type="b", ...)
   n.max <- which.max(t)
   v.max <- max(t, na.rm=TRUE)
   u <- par()$usr
