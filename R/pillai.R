b5222b7196875c61c3a84c33ba33136f77245d68
Jianan Tian <jiant17@gmail.com>
2014-06-11 16:44:47 -0500

    deal with complex eigen values.

--- R/pillai.R
+++ R/pillai.R
@@ -1,5 +1,9 @@
 pillai <- function(S1, S0inv){
   A <- crossprod(S1, S0inv)
   lambda <- eigen(A)$values
+  if(is.complex(lambda)){
+    if(all(abs(Im(lambda)) < 1e-7)) lambda <- Re(lambda)
+    else return(NA)
+  } 
   return(sum(1/(1+lambda)))
 }
