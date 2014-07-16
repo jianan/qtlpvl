64d2fddfb0904c9bcb45e2b511dbe0a4d19683dd
Jianan Tian <jiant17@gmail.com>
2014-07-16 17:14:58 -0500

    add geno and pheno infomation to transbands

---
+++ R/add.geno.R
@@ -0,0 +1,31 @@
+##' find the peak of scanone.mvn and the QTL genotype.
+add.geno <- function(transbands, cross, regn.cM=5, max.p=100){
+  
+  for(i in 1:length(transbands)){  
+    chr <- attr(transbands[[i]], "info")$chr
+    Y <- attr(transbands[[i]], "Y")
+    
+    ## scan the first 100.
+    Y <- Y[, 1:min(max.p, ncol(Y))]
+    
+    out.scan1 <- scanone.mvn(cross, Y, chr=chr)
+    pos <- max(out.scan1)$pos
+    pmarker <- find.pseudomarker(cross, chr=chr, pos=pos, where="prob")
+    genoprob <- pull.genoprob(cross, chr=chr)
+
+    genoprob.pmarker <- genoprob[, grep(pmarker, colnames(genoprob))]
+    geno <- apply(genoprob.pmarker, 1, which.max)
+
+    pmarker1 <- find.pseudomarker(cross, chr=chr, pos=pos-regn.cM, where="prob")
+    pmarker2 <- find.pseudomarker(cross, chr=chr, pos=pos+regn.cM, where="prob")
+    geno1 <- apply(genoprob[, grep(pmarker1, colnames(genoprob))], 1, which.max)
+    geno2 <- apply(genoprob[, grep(pmarker2, colnames(genoprob))], 1, which.max)
+    nonrecomb <- which(geno==geno1 & geno==geno2)
+    
+    attr(transbands[[i]], "geno") <- geno
+    attr(transbands[[i]], "nonrecomb") <- nonrecomb
+    attr(transbands[[i]], "info")$pos <- pos
+    attr(transbands, "trans.info")$pos[i] <- pos
+  }
+  return(transbands)
+}
