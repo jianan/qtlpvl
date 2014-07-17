acf5549770fe69c897796564f2dfe153487e45ab
Jianan Tian <jiant17@gmail.com>
2014-07-17 11:56:05 -0500

    use gray plot for PCA and LDA

--- R/plottrans.LDA.R
+++ R/plottrans.LDA.R
@@ -13,7 +13,33 @@ plottrans.LDA <- function(Y, geno, nonrecomb, max.p=100, ...){
   sca <- fit$scaling
   y.lda <- Y %*% sca
 
-  plot(y.lda, col=geno, xlab="LD1", ylab="LD2", ...)
-  points(y.lda[nonrecomb, ], pch=20, col=geno[nonrecomb])
-  points(y.lda[-nonrecomb, ], pch=20, col="yellow")
+  orange <- rgb(230, 159, 0, maxColorValue = 256)
+  green <- rgb(27, 159, 120, maxColorValue = 256)
+  blue <- rgb(123, 104, 238, maxColorValue = 256)
+  yellow <- rgb(255, 255, 0, maxColorValue = 256)
+  genecolor <- c(blue,orange,green, yellow)
+
+  Class <- geno
+  Class[-nonrecomb] <- 4
+  xlim <- range(y.lda[, 1])
+  ylim <- range(y.lda[, 2])
+  px <- pretty(xlim)
+  py <- pretty(ylim)
+  grayplot(x=y.lda[,1],y=y.lda[,2],
+           pch=21,xat=px,yat=py,col="black",bg=genecolor[Class],
+           hlines=py, vlines=px,
+           xaxt="n", yaxt="n",
+           xaxs="r", yaxs="r",
+           xlim=xlim, ylim=ylim,
+           xlab="Linear Discriminant 1", ylab="Linear Discriminant 2",
+           mgp=c(1.6,0,0), cex=0.8, ...)
+
+  u <- par("usr")
+  x <- u[1] + diff(u[1:2])*((2:5)*0.1+0.05)
+  points(x, rep(u[4]+diff(u[3:4])*0.035, 4), pch=21, col="black",
+         bg=genecolor, xpd=TRUE, cex=0.8)
+  x <- u[1] + diff(u[1:2])*((2:5)*0.1+0.05)
+  text(x, rep(u[4]+diff(u[3:4])*0.035, 3), c("BB","BR","RR","Recombinants"),
+       col=c(genecolor[1:3],"black"), xpd=TRUE, cex=0.8, pos=4)
+
 }
--- R/plottrans.PCA.R
+++ R/plottrans.PCA.R
@@ -13,7 +13,32 @@ plottrans.PCA <- function(Y, geno, nonrecomb, max.p=100, ...){
 
   PC <- predict(princomp(Y))[, 1:2]
   
-  plot(PC, xlab="PC1", ylab="PC2", ...)
-  points(PC[-nonrecomb, ], pch=20, col="yellow")
-  points(PC[nonrecomb, ], pch=20, col=geno[nonrecomb])
+  orange <- rgb(230, 159, 0, maxColorValue = 256)
+  green <- rgb(27, 159, 120, maxColorValue = 256)
+  blue <- rgb(123, 104, 238, maxColorValue = 256)
+  yellow <- rgb(255, 255, 0, maxColorValue = 256)
+  genecolor <- c(blue,orange,green, yellow)
+
+  Class <- geno
+  Class[-nonrecomb] <- 4
+  xlim <- range(PC[, 1])
+  ylim <- range(PC[, 2])
+  px <- pretty(xlim)
+  py <- pretty(ylim)
+  grayplot(x=PC[,1],y=PC[,2],
+           pch=21,xat=px,yat=py,col="black",bg=genecolor[Class],
+           hlines=py, vlines=px,
+           xaxt="n", yaxt="n",
+           xaxs="r", yaxs="r",
+           xlim=xlim, ylim=ylim,
+           xlab="Principal component 1", ylab="Principal component 2",
+           mgp=c(1.6,0,0), cex=0.8, ...)
+
+  u <- par("usr")
+  x <- u[1] + diff(u[1:2])*((2:5)*0.1+0.05)
+  points(x, rep(u[4]+diff(u[3:4])*0.035, 4), pch=21, col="black",
+         bg=genecolor, xpd=TRUE, cex=0.8)
+  x <- u[1] + diff(u[1:2])*((2:5)*0.1+0.05)
+  text(x, rep(u[4]+diff(u[3:4])*0.035, 3), c("BB","BR","RR","Recombinants"),
+       col=c(genecolor[1:3],"black"), xpd=TRUE, cex=0.8, pos=4)
 }
