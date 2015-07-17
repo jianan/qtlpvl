67f8c02db0258ace63e49819cbe03200ee66e4c7
Jianan Tian <jiant17@gmail.com>
2015-07-17 10:11:50 -0500

    use all markers to tell non-recomb

--- R/add.geno.R
+++ R/add.geno.R
@@ -1,30 +1,34 @@
 ##' find the peak of scanone.mvn and the QTL genotype.
+##'
+##' For a list of hotspots, find the single QTL genotype and the mice
+##' with no recombinant events in a 10cM region around the single QTL.
+##'
+##' @param regn.cM disctance in cM form QTL for defining non-recombinanct mice.
+##'
 add.geno <- function(transbands, cross, regn.cM=5, max.p=100){
   allele <- attr(cross, "alleles")
   gt <- c(paste0(allele[1], allele[1]),
           paste0(allele[1], allele[2]),
           paste0(allele[2], allele[2]))
-  for(i in 1:length(transbands)){  
+  for(i in 1:length(transbands)){
     chr <- attr(transbands[[i]], "info")$chr
     Y <- attr(transbands[[i]], "Y")
-    
+
     ## scan the first 100.
     Y <- Y[, 1:min(max.p, ncol(Y))]
-    
+
     out.scan1 <- scanone.mvn(cross, Y, chr=chr)
     pos <- max(out.scan1)$pos
     pmarker <- find.pseudomarker(cross, chr=chr, pos=pos, where="prob")
     genoprob <- pull.genoprob(cross, chr=chr)
-
     genoprob.pmarker <- genoprob[, paste(pmarker, gt, sep=":")]
     geno <- apply(genoprob.pmarker, 1, which.max)
 
-    pmarker1 <- find.pseudomarker(cross, chr=chr, pos=pos-regn.cM, where="prob")
-    pmarker2 <- find.pseudomarker(cross, chr=chr, pos=pos+regn.cM, where="prob")
-    geno1 <- apply(genoprob[, paste(pmarker1, gt, sep=":")], 1, which.max)
-    geno2 <- apply(genoprob[, paste(pmarker2, gt, sep=":")], 1, which.max)
-    nonrecomb <- which(geno==geno1 & geno==geno2)
-    
+    m <- which(out.scan1$pos >= pos-regn.cM & out.scan1$pos <= pos+regn.cM)
+    g <- apply(cross$geno[[chr]]$prob[,m,], 1:2, which.max)
+    nonrecomb <- which(sapply(apply(g, 1, unique), length) == 1)
+    names(nonrecomb) <- rownames(Y)[nonrecomb]
+
     attr(transbands[[i]], "geno") <- geno
     attr(transbands[[i]], "nonrecomb") <- nonrecomb
     attr(transbands[[i]], "info")$pos <- pos
