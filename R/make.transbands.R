ab3e98c2055e4e86a80a723dfb520dc425ac333f
Jianan Tian <jiant17@gmail.com>
2015-07-17 10:12:33 -0500

    doc parameters

--- R/make.transbands.R
+++ R/make.transbands.R
@@ -1,15 +1,16 @@
 ##' From a scan1 result, first count how many trans-eQTL are there in
 ##' every bin across the chromosomes, then find phenos inside each
 ##' transband. Add all pheno and geno infomations needed.
-##' 
+##'
 ##' @inheritParams find.trans
+##' @inheritParams add.geno
 ##' @inheritParams scanone.mvn
 ##' @return a list of transband.
 ##' @export
-make.transbands <- function(out1, probepos, cross, chr, marker.info, mlratio, 
+make.transbands <- function(out1, probepos, cross, chr, marker.info, mlratio,
                             lod.thr=5, trans.cM=5, kernal.width=1, window.cM=10,
                             trans.count.thr=300, regn.cM=5){
-  transbands <- find.trans(out1, probepos, chr, marker.info, 
+  transbands <- find.trans(out1, probepos, chr, marker.info,
                            lod.thr=lod.thr, trans.cM=trans.cM,
                            kernal.width=kernal.width, window.cM=window.cM,
                            trans.count.thr=trans.count.thr)
