5dd7c3f0f00fd58192932718ae98614f7072981f
Karl Broman <kbroman@gmail.com>
2015-08-05 23:49:44 -0500

    Revise DESCRIPTION to avoid warnings/notes in R CMD check

--- R/qtlpvl-package.R
+++ R/qtlpvl-package.R
@@ -1,10 +1,9 @@
 ##' qtlpvl
-##' 
+##'
 ##' QTL: test of pleiotrophy vs. close linkage
 ##'
 ##' @import qtl
 ##' @import Rcpp
-##' @import RcppEigen
 ##' @name qtlpvl
 ##' @useDynLib qtlpvl
 ##' @docType package
